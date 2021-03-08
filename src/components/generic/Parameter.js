927cf5bc37f5d8c22f69ac2cd78f3dc1c0d2de7e
michaelKosatka <michael.kosatka@seznam.cz>
2021-03-08 14:30:16 +0100

    optional Icon rendering implemented

--- src/components/generic/Parameter.js
+++ src/components/generic/Parameter.js
@@ -1,11 +1,24 @@
 import React from 'react';
 
-const Parameter = ({ className, label, value, units }) => {
+const Parameter = ({ className, label, value, units, icon }) => {
+	const renderIcon = () => {
+		if (icon) {
+			const Icon = icon.icon;
+			return (
+				<Icon
+					className="icon"
+					style={{ transform: `rotate(${icon.dir + 180}deg)` }}
+				/>
+			);
+		}
+	};
+
 	return (
 		<div className={className}>
 			<span className="label">{label}: </span>
 			<span className="value">{value} </span>
 			<span className="units">{units}</span>
+			{renderIcon()}
 		</div>
 	);
 };
