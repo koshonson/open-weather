776d8f4dd70be06a2681675a5defddd3dedaf55f
michaelKosatka <michael.kosatka@seznam.cz>
2021-03-08 13:21:45 +0100

    TemperatureBox component implementation

---
+++ src/components/generic/TemperatureBox.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const TemperatureBox = ({ className, tC, tF, units, switchUnits }) => {
+	console.log(tC, tF);
+	return (
+		<div className={className}>
+			<div className="value">{units === 'celsius' ? tC : tF}</div>
+			<div className="unit" onClick={switchUnits}>
+				{units === 'celsius' ? '° C' : '° F'}
+			</div>
+		</div>
+	);
+};
+
+export default TemperatureBox;
